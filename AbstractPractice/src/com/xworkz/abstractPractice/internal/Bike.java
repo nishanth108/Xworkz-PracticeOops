99ea0a5a284b6b3a90b1c201d169453d91fab55e
Nishanth <nishanthkumarkl14@gmail.com>
2025-04-15 22:46:21 +0530

    commiting the Abstract methods

--- AbstractPractice/src/com/xworkz/abstractPractice/internal/Bike.java
+++ AbstractPractice/src/com/xworkz/abstractPractice/internal/Bike.java
@@ -2,7 +2,7 @@ package com.xworkz.abstractPractice.internal;
 
 public abstract class Bike {
     public void sound() {
-        System.out.println("The sound is excellent but need new exhuast");
+        System.out.println("The sound is excellent but need new  exhuast");
     }
     public abstract void changeExhaust();
 }
